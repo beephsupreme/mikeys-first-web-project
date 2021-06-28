8c62c453a8b68b7e87cd9b148fb387a4d2bbf843
beephsupreme <beephsupreme@gmail.com>
2021-06-27 18:24:13 -0700

    works - needs more review

--- email_list.js
+++ email_list.js
@@ -1,25 +1,41 @@
 const $ = function (id) {
     return document.getElementById(id);
 };
-var joinList = function () {
-    var emailAddress1 = $("mail_address1").value;
-    var emailAddress2 = $("mail_address2").value;
-    var isValid = true;
 
-    if (emailAddress1 == "") {
-        $("mail_address1_error").firstChild.nodeValue = "This field is required.";
+const joinList = function () {
+    const emailAddress1 = $("email_address1").value;
+    const emailAddress2 = $("email_address2").value;
+    const firstName = $("first_name").value;
+    let isValid = true;
+
+    if (emailAddress1 === "") {
+        $("email_address1_error").firstChild.nodeValue = "This field is required.";
         isValid = false;
     } else {
-        $("mail_address_error").firstChild.nodeValue = "";
+        $("email_address1_error").firstChild.nodeValue = "";
     }
-
+    if (emailAddress2 === "") {
+        $("email_address2_error").firstChild.nodeValue = "Required";
+        isValid = false;
+    } else if (emailAddress1 !== emailAddress2) {
+        $("email_address2_error").firstChild.nodeValue = "Does Not Match";
+        isValid = false;
+    } else {
+        $("email_address2_error").firstChild.nodeValue = "";
+    }
+    if (firstName === "") {
+        $("first_name_error").firstChild.nodeValue = "Required";
+        isValid = false;
+    } else {
+        $("first_name_error").firstChild.nodeValue = "";
+    }
 
     if (isValid) {
         $("email_form").submit();
     }
 };
-window.onLoad = function () {
 
-    $("join_list").onClick = joinList;
+window.onload = function () {
+    $("join_list").onclick = joinList;
     $("email_address1").focus();
 };
